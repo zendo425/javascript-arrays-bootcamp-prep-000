49fe6f3b91f2adc6202ca147543bbe7edc59077a
Demetrious Young <zendo425@gmail.com>
2018-01-10 01:37:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
+    array.push(element);
   return array;
 }
 
