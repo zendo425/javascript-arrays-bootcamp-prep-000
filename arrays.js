f6990a2b8d8e090dee6690df576b2d8d02d9f103
Demetrious Young <zendo425@gmail.com>
2018-01-10 01:31:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,25 @@ function destructivelyRemoveElementFromEndOfArray(array){
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length - 1);
 }
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1);
+}
