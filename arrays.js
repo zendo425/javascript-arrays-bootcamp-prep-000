6f8a2c1ed5cf6e0a91440924742ecc5eb29e96c5
Demetrious Young <zendo425@gmail.com>
2018-01-10 00:53:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,5 @@
 var chocolateBars = ["chocolatBars", "snickers", "hundred grand", "kit kat", "skittles"];
 
-function addElementToBeginningOfArray = ("porsche", "mercedes") {
-  
-   addElementToBeginningOfArray.concat("corvette");
-};
-
-var destructivelyAddElementToBeginningOfArray = ["ford", "chevrolet"];
-
-destructivelyAddElementToBeginningOfArray.unshift("dodge");
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
