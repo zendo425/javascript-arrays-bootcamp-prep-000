4e3cf81a3cc2504566323ad5fda3778f82351dcb
Demetrious Young <zendo425@gmail.com>
2018-01-10 01:36:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -53,7 +53,7 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
+  function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
